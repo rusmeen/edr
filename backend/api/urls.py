602ceea3ec658b611a765e74c5eef5421c8a5f3b
lalit97 <sutharlalit.97@gmail.com>
2022-12-10 00:48:41 +0530

    feat: Authentication apis

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("api/v1/users/", views.UserListAPI.as_view()),
-    path("api/v1/users/all",views.CategoryList.as_view()),
-    path("api/v1/category/<pk>",views.by_id),
+    path("api/v1/users/all", views.CategoryList.as_view()),
+    path("api/v1/category/<pk>", views.by_id),
+    path("api/v1/register/", views.RegisterApi.as_view()),
+    path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,15 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser 
+from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 
-from . models import Category_schema
-from . serializer import categorySerializer
+from .models import Category_schema
+from .serializer import categorySerializer
 
 from rest_framework.decorators import api_view
+from rest_framework import generics
+from api.serializers import RegisterSerializer, UserSerializer
 
 
 class UserListAPI(APIView):
@@ -16,64 +18,71 @@ class UserListAPI(APIView):
         return Response(users, status=status.HTTP_200_OK)
 
 
-    #################################################################################################
+class RegisterApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user_data = UserSerializer(user, context=self.get_serializer_context()).data
+        return Response(
+            user_data,
+            status=status.HTTP_201_CREATED,
+        )
+
+
 ##################### Category APIs: Start ###############################################################
-    #################################################################################################
+#################################################################################################
 category = []
 
-class CategoryList(APIView):
-    
-    def get(self,requset):
-        category1 = Category_schema.objects.all()
-        serializer = categorySerializer(category1,many=True)
-        return Response(serializer.data) 
 
-    def post(self,request):
+class CategoryList(APIView):
+    def get(self, requset):
+        category1 = Category_schema.objects.all()
+        serializer = categorySerializer(category1, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
         category_data = JSONParser().parse(request)
         category_serializer = categorySerializer(data=category_data)
         if category_serializer.is_valid():
             category_serializer.save()
-            return JsonResponse(category_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(category_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(
+                category_serializer.data, status=status.HTTP_201_CREATED
+            )
+        return JsonResponse(
+            category_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
+@api_view(["GET", "PUT", "DELETE"])
+def by_id(request, pk):
+    try:
+        category = Category_schema.objects.get(pk=pk)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def by_id( request,pk):
-    try: 
-        category =  Category_schema.objects.get(pk=pk) 
-        
-    except Category_schema.DoesNotExist: 
-                return JsonResponse({'message': 'The Category does not exist'}, status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET': 
-        category_serializer = categorySerializer(category) 
-        return JsonResponse(category_serializer.data)  
-   
-    elif request.method == 'PUT': 
-        category_data = JSONParser().parse(request) 
-        category_serializer = categorySerializer(category, data=category_data) 
-        if category_serializer.is_valid(): 
-            category_serializer.save() 
-            return JsonResponse(category_serializer.data) 
-        return JsonResponse(category_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+    except Category_schema.DoesNotExist:
+        return JsonResponse(
+            {"message": "The Category does not exist"}, status=status.HTTP_404_NOT_FOUND
+        )
 
-    elif request.method == 'DELETE': 
-        category.delete() 
-        return JsonResponse({'message': 'category was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)   
+    if request.method == "GET":
+        category_serializer = categorySerializer(category)
+        return JsonResponse(category_serializer.data)
 
+    elif request.method == "PUT":
+        category_data = JSONParser().parse(request)
+        category_serializer = categorySerializer(category, data=category_data)
+        if category_serializer.is_valid():
+            category_serializer.save()
+            return JsonResponse(category_serializer.data)
+        return JsonResponse(
+            category_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
 
-    
-           
-   
-
-    
-    
-
-
-
-
-    
-    
-
-    
+    elif request.method == "DELETE":
+        category.delete()
+        return JsonResponse(
+            {"message": "category was deleted successfully!"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
